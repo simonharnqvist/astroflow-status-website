250c1c13966941e780eebe0f3359e38f9b594975
simonharnqvist <45334810+simonharnqvist@users.noreply.github.com>
2026-02-26 12:04:06 +0000

    Create upload_html.tf

---
+++ upload_html.tf
@@ -0,0 +1,15 @@
+# Upload a simple index.html page to the bucket
+resource "google_storage_bucket_object" "indexpage" {
+  name         = "index.html"
+  content      = "<html><body>Hello World!</body></html>"
+  content_type = "text/html"
+  bucket       = google_storage_bucket.static_website.id
+}
+
+# Upload a simple 404 / error page to the bucket
+resource "google_storage_bucket_object" "errorpage" {
+  name         = "404.html"
+  content      = "<html><body>404!</body></html>"
+  content_type = "text/html"
+  bucket       = google_storage_bucket.static_website.id
+}
